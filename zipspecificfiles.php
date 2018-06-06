5af3b9d7b743fcf5fd97ada27c711853acfd013a
Taoufiq Ait Ali <23a.marketing@gmail.com>
2018-06-06 10:39:47 +0000

    Create zipspecificfiles.php

---
+++ zipspecificfiles.php
@@ -0,0 +1,64 @@
+<?php
+set_time_limit(0);
+ini_set('memory_limit', '500M');
+error_reporting(1);
+echo "ok";
+
+
+/* creates a compressed zip file */
+function create_zip($files = array(),$destination = '',$overwrite = false) {
+  //if the zip file already exists and overwrite is false, return false
+  if(file_exists($destination) && !$overwrite) { return false; }
+  //vars
+  $valid_files = array();
+  //if files were passed in...
+  if(is_array($files)) {
+    //cycle through each file
+    foreach($files as $file) {
+      //make sure the file exists
+      if(file_exists($file)) {
+        $valid_files[] = $file;
+      }
+    }
+  }
+  //if we have good files...
+  if(count($valid_files)) {
+    //create the archive
+    $zip = new ZipArchive();
+    if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+      return false;
+    }
+    //add the files
+    foreach($valid_files as $file) {
+      $zip->addFile($file,$file);
+    }
+    //debug
+    //echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+    
+    //close the zip -- done!
+    $zip->close();
+    
+    //check to make sure the file exists
+    return file_exists($destination);
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+
+
+$files_to_zip = array(
+  'admin557/themes/default/template/controllers/orders/_product_line.tpl',
+'modules/themeconfigurator/views/templates/admin/new.tpl',
+'pdf/invoice.tpl'
+);
+//if true, good; if false, zip creation failed
+$result = create_zip($files_to_zip,'my-archive.zip',true);
+
+
+echo "ok zipped";
+
+?>
