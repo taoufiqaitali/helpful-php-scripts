dfdd1abfca2df4acc4359cf5b66eb69879c4f601
Taoufiq Ait Ali <23a.marketing@gmail.com>
2018-07-16 12:23:52 +0100

    Update downloadviaurl.php

--- downloadviaurl.php
+++ downloadviaurl.php
@@ -5,6 +5,7 @@ ini_set('memory_limit', '512M');
 error_reporting(1);
 //https://wordpress.org/latest.zip
 //https://api.prestashop.com/xml/channel.xml
+//https://api.prestashop.com/xml/channel17.xml
 $content = file_get_contents('http://vorboss.dl.sourceforge.net/project/extplorer/eXtplorer_2.1.7.zip');
 file_put_contents('filemanager.zip', $content);
 echo 'ok';
